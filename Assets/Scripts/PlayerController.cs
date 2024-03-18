b94858295c6e92e906a87060657ae4cbeee4307a
SHsus1122 <tjgurtn1122@naver.com>
2024-03-19 00:55:03 +0900

    기본 조작 및 카메라 회전 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        // 플레이어(구체)가 바라보는 방향이 아닌 카메라가 바라보는 방향으로 이동하기 위해서 하는 작업
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Vector3 = UnityEngine.Vector3;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed; // 레벨 회전 속도
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // InputManager 를 이용해서 레벨의 회전에 대한 처리입니다.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
