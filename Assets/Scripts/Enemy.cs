ee05aba7d815bbfbe0b343d56991bfe1ee7c9b3e
SHsus1122 <tjgurtn1122@naver.com>
2024-03-19 10:51:39 +0900

    모듈화를 통한 랜덤 스폰 스크립트 추가

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player 의 위치로부터 enemy 위치까지의 거리만큼 원하는 속도로 계속해서 힘을 주어 이동시킵니다.(플레이어 추적 및 이동)
+        // 시간이 지남에 따라 값이 너무 커져서 힘이 너무 커지는 즉, 거리에 영향을 너무 많이 받지 않게끔 해줍니다.(normalized)
+        // normalized 를 플레이어와 적 사이의 거리를 구한 Vector3 뒤에 입력해주면, 벡터의 크기를 정규화 합니다.
+        // 이를 통해 적과 플레이어 간의 거리에 상관없이 항상 일정한 속도로 움직이게 됩니다.(방향성만 구하고 크기는 1로 고정)
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 랜덤 스폰 코드
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 랜덤 적 스폰 커스텀 메서드
+    // 내부에서만 사용할 함수이기에 private 접근자 사용
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);  // 랜덤 스폰 X 0~9
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);  // 랜덤 스폰 Z 0~9
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ); // 랜덤 스폰 좌표
+        return randomPos;
+    }
+}
